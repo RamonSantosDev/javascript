a857b78d84ffca5f7545367e2d026a1c87570134
Ramon Nascimento <ramon.dev.rs@gmail.com>
2026-06-07 22:21:32 -0300

    ex 8, 9 , 10 , 11

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70 
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { // condição simples!
+    console.log('Voce foi MULTADO!')
+}
+console.log('Digija sempre usando cinto de segurança!')
